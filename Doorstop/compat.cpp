c46aaba68ae3521717c27c7eefa1b38a2088f69a
VeraDev <159570832+VeraDev0@users.noreply.github.com>
2025-12-11 22:36:13 +0100

    fix: remove unneeded mygui log patch
    Closes #1

--- Doorstop/compat.cpp
+++ Doorstop/compat.cpp
@@ -61,29 +61,6 @@ void Compat::InitializeFunctionPointers() {
 	GET_REAL_FUNC_PTR(VerInstallFileW);
 }
 
-void Compat::PatchMyGUI() {
-	HMODULE hMyGUI = LoadLibraryW(L"MyGUIEngine.dll");
-	if (!hMyGUI) {
-		return;
-	}
-
-	// Get `struct tm *__fastcall MyGUI::LogManager::log(_QWORD **a1, int a2, int a3, __int64 a4, __int64 a5, int a6)`
-	PVOID pLogFunc = reinterpret_cast<PVOID>(GetProcAddress(hMyGUI, "?log@LogManager@MyGUI@@QEAAXAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@ULogLevel@2@0PEBDH@Z"));
-	if (!pLogFunc)
-		return;
-
-	// Stick a `ret` at the start of the function to noop it out.. it crashes for some reason
-	// TODO: Redirect to the games logging system instead of doing this....
-	// See issue #1 on GitHub for more info.
-
-	DWORD oldProtect;
-	VirtualProtect(pLogFunc, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
-	unsigned char retInstr = 0xC3; // x64 ret instruction
-	memcpy(pLogFunc, &retInstr, 1);
-	VirtualProtect(pLogFunc, 1, oldProtect, &oldProtect);
-	FlushInstructionCache(GetCurrentProcess(), pLogFunc, 1);
-}
-
 using AllocConsoleFunc = std::add_pointer_t<decltype(AllocConsole)>;
 AllocConsoleFunc oAllocConsole = nullptr;
 
@@ -157,6 +134,5 @@ void Rivet::Compat::PatchAllocConsole() {
 
 void Compat::Initialize() {
 	InitializeFunctionPointers();
-	PatchMyGUI();
 	PatchAllocConsole();
 }
--- Doorstop/compat.h
+++ Doorstop/compat.h
@@ -5,7 +5,6 @@
 namespace Rivet {
 	namespace Compat {
 		void InitializeFunctionPointers();
-		void PatchMyGUI();
 		void PatchAllocConsole();
 		void Initialize();
 	}
