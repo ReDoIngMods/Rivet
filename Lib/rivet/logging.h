9e2c8986b29ca4683c959202468ce17665102bcf
VeraDev <159570832+VeraDev0@users.noreply.github.com>
2025-12-13 11:13:29 +0100

    fix: fix logging.h for rivet lib

--- Lib/rivet/logging.h
+++ Lib/rivet/logging.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "includes.h"
-#include "logger.h"
 #include "sharedmacros.h"
 #include <mutex>
 
 namespace Rivet {
+	enum class LogLevel {
+		Info,
+		Warning,
+		Error,
+		Debug
+	};
+
+	class Logger {
+		std::string_view name_;
+
+		void _log(Rivet::LogLevel logLevel, const std::string_view& format, va_list arguments);
+	public:
+		Logger(const std::string_view& name);
+
+		RIVET_DOORSTOP_API void info(const std::string_view format, ...);
+		RIVET_DOORSTOP_API void warn(const std::string_view format, ...);
+		RIVET_DOORSTOP_API void error(const std::string_view format, ...);
+		RIVET_DOORSTOP_API void debug(const std::string_view format, ...);
+	};
+
 	class LoggerManager {
 		LoggerManager() = delete;
 		LoggerManager(const LoggerManager&) = delete;
